523cd7d88262436edb33b3c37861bbd7722d2c7c
Mo Dong <dongmo1024@outlook.com>
2025-05-27 16:38:41 -0700

    sample pd codes after setting up virtual env

--- example.py
+++ example.py
@@ -6,3 +6,17 @@ print(msg)
 
 
 print("test")
+
+import pandas as pd
+
+s = pd.Series([1, 3, 5, None, 6, 8])
+print("Pandas Series:")
+print(s)
+
+# Creating a DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 28],
+        'City': ['New York', 'London', 'Paris']}
+df = pd.DataFrame(data)
+print("\nPandas DataFrame:")
+print(df)
